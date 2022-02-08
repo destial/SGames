7a9b8c6368170dd178d9cc7a554ff4081635e264
destiall <destialpvp@gmail.com>
2022-02-08 23:20:08 +0800

    Update Spectator.java

--- src/main/java/xyz/destiall/sgames/player/Spectator.java
+++ src/main/java/xyz/destiall/sgames/player/Spectator.java
@@ -1,7 +1,6 @@
 package xyz.destiall.sgames.player;
 
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
@@ -9,6 +8,7 @@ import org.bukkit.util.Vector;
 import xyz.destiall.sgames.api.SGPlayer;
 import xyz.destiall.sgames.match.Match;
 
+import java.util.Arrays;
 import java.util.Collection;
 
 public class Spectator extends SGPlayer {
@@ -39,30 +39,15 @@ public class Spectator extends SGPlayer {
 
     public void copyInventory(PlayerInventory other) {
         if (player != null) {
-            ItemStack[] otherContents = other.getContents();
-            ItemStack[] selfContents = getInventory().getContents();
-            for (int i = 0; i < selfContents.length; i++) {
-                if (selfContents[i] == null && otherContents[i] == null) continue;
-                if (selfContents[i] != null && otherContents[i] == null) {
-                    getInventory().setItem(i, null);
-                    continue;
-                }
-                if (selfContents[i] == null && otherContents[i] != null) {
-                    getInventory().setItem(i, otherContents[i]);
-                    continue;
-                }
-                if (selfContents[i].equals(otherContents[i])) continue;
-
-                if (selfContents[i].isSimilar(otherContents[i])) {
-                    getInventory().getItem(i).setAmount(otherContents[i].getAmount());
-                    continue;
-                }
-                getInventory().getItem(i).setType(otherContents[i].getType());
-                getInventory().getItem(i).setAmount(otherContents[i].getAmount());
+            if (!Arrays.equals(player.getInventory().getContents(), other.getContents())) {
+                player.getInventory().setContents(other.getContents());
             }
-            // player.getInventory().setContents(otherContents);
-            player.getInventory().setArmorContents(other.getArmorContents());
-            player.getInventory().setExtraContents(other.getExtraContents());
+            if (!Arrays.equals(player.getInventory().getArmorContents(), other.getArmorContents())) {
+                player.getInventory().setArmorContents(other.getArmorContents());
+            }
+            if (!Arrays.equals(player.getInventory().getExtraContents(), other.getExtraContents())) {
+                player.getInventory().setExtraContents(other.getExtraContents());
+            }
         }
     }
 
